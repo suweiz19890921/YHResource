3f8273f337548d1b441a89ce589abf725a2ba695
苏威 <suwei@solot.com>
2017-04-22 21:04:38 +0800

    change

--- Example/Pods/Target Support Files/YHResource/YHResource-umbrella.h
+++ Example/Pods/Target Support Files/YHResource/YHResource-umbrella.h
@@ -2,6 +2,8 @@
 #import <UIKit/UIKit.h>
 #endif
 
+#import "YHResourceCacheManager.h"
+#import "YHResourceModel.h"
 
 FOUNDATION_EXPORT double YHResourceVersionNumber;
 FOUNDATION_EXPORT const unsigned char YHResourceVersionString[];
